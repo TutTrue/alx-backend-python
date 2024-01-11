5df1e56e31faf3db93b8e8719acadca4e3bf21f4
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-11 14:46:34 +0200

    use variable annotation

--- 0x00-python_variable_annotations/4-define_variables.py
+++ 0x00-python_variable_annotations/4-define_variables.py
@@ -2,7 +2,7 @@
 """ This module defines global variables """
 
 
-a = 1
-pi = 3.14
-i_understand_annotations = True
-school = "Holberton"
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
