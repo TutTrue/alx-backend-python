bef845daf4a3e3c360e7be726d0da2c7ca12128a
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-15 16:25:43 +0200

    remove list comp

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -7,8 +7,10 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Waits for a random delay between 0 and max_delay """
-    li = [await wait_random(max_delay) for _ in range(n)]
-    res = []
+    li: List[asyncio.Future[float]] = []
+    for i in range(n):
+        li.append(wait_random(max_delay))
+    res: List[float] = []
     for i in asyncio.as_completed(li):
         res.append(await i)
     return res
