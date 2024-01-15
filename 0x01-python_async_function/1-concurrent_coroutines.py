911df67444db323ddb7a41fb6ba1d70a132ffaaf
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-15 15:43:08 +0200

    add typing annotations

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,11 +1,9 @@
 #!/bin/usr/env python3
 """ The basics of async """
+from typing import List
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-async def wait_n(n, max_delay):
+async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Waits for a random delay between 0 and max_delay """
-    list_delays = []
-    for i in range(n):
-        list_delays.append(await wait_random(max_delay))
-    return sorted(list_delays)
+    return sorted([await wait_random(max_delay) for _ in range(n)])
