0e45f5a5d73f141dcff610ca8fc77333835293fb
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-15 15:39:27 +0200

    regular for loop instead of list comp

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -5,4 +5,7 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 
 async def wait_n(n, max_delay):
     """ Waits for a random delay between 0 and max_delay """
-    return sorted([await wait_random(max_delay) for _ in range(n)])
+    list_delays = []
+    for i in range(n):
+        list_delays.append(await wait_random(max_delay))
+    return sorted(list_delays)
