22262726bc62206601d6b2ec1d3f4e3a6eb25c02
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-15 16:53:43 +0200

    [docs] typing annotation

--- 0x01-python_async_function/3-tasks.py
+++ 0x01-python_async_function/3-tasks.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 """ The basics of async """
 import asyncio
-import random
 from typing import List
 
 
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> List[float]:
+def task_wait_random(max_delay: int) -> asyncio.Task:
     """
         @max_delay: max delay of wait_random
         Return: asyncio.Task
     """
-    return asyncio.create_task(wait_random(max_delay))
+    return asyncio.Task(wait_random(max_delay))
