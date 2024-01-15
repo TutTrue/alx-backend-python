b689149e20b23782b832c31687e1a1ca6b1a7591
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-15 16:15:28 +0200

    use as_completed()

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,9 +1,14 @@
 #!/bin/usr/env python3
 """ The basics of async """
 from typing import List
+import asyncio
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
     """ Waits for a random delay between 0 and max_delay """
-    return [await wait_random(max_delay) for _ in range(n)]
+    li = [await wait_random(max_delay) for _ in range(n)]
+    res = []
+    for i in asyncio.as_completed(li):
+        res.append(await i)
+    return res
