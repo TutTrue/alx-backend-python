e0b6ac9088954171e04b8c0b5b525957d1f916f7
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-01-16 17:00:23 +0200

    [docs] task 0 typing annotation generator returns Generator[float, None, None] not a float

--- 0x02-python_async_comprehension/0-async_generator.py
+++ 0x02-python_async_comprehension/0-async_generator.py
@@ -5,7 +5,7 @@ import random
 from typing import Generator
 
 
-async def async_generator() -> float:
+async def async_generator() -> Generator[float, None, None]:
     """ Async Generator """
     for _ in range(10):
         await asyncio.sleep(1)
