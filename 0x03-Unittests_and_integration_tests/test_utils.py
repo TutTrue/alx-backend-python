56f1b133d261c74e25bc9036b53aa895e88113c4
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-06 17:25:39 +0200

    run unittest if file test_utils.py is main

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -18,3 +18,7 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+
+if __name__ == "__main__":
+    unittest.main()
