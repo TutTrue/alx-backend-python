b8d0c843bc82e5e39e0dd0cbc32df85c75ddfccd
Mahmoud Hamdy <hamdy.xd55@gmail.com>
2024-02-06 17:31:31 +0200

    Use the assertRaises test_utils.py

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -8,7 +8,7 @@ from utils import access_nested_map
 class TestAccessNestedMap(unittest.TestCase):
     """Test access_nested_map"""
 
-    @Parameterized.expand(
+    @parameterized.expand(
         [
             ({"a": 1}, "a", 1),
             ({"a": {"b": 2}}, ("a",), {"b": 2}),
@@ -18,6 +18,17 @@ class TestAccessNestedMap(unittest.TestCase):
     def test_access_nested_map(self, nested_map, path, expected):
         """Test access_nested_map"""
         self.assertEqual(access_nested_map(nested_map, path), expected)
+
+    @parameterized.expand(
+        [
+            ({}, ("a",), KeyError),
+            ({"a": 1}, ("a", "b"), KeyError),
+        ]
+    )
+    def test_access_nested_map_exception(self, nested_map, path, expected):
+        """Test access_nested_map"""
+        with self.assertRaises(expected):
+            access_nested_map(nested_map, path)
 
 
 if __name__ == "__main__":
